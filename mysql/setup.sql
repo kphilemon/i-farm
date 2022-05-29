1c426d8a2dc261d3ed496c3efd7609a2b6d2e170
nic-00 <55591460+nic-00@users.noreply.github.com>
2022-05-29 23:19:32 +0800

    Add create table statements for pesticide, fertilizer, farm_fertilizer, farm_pesticide

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -24,3 +24,33 @@ CREATE TABLE farm_plant
     FOREIGN KEY (`farm_id`) REFERENCES farm (`id`),
     FOREIGN KEY (`plant_id`) REFERENCES plant (`id`)
 );
+
+CREATE TABLE fertilizer
+(
+    `id`        VARCHAR(255) PRIMARY KEY,
+    `name`      VARCHAR(255) NOT NULL,
+    `unit_type` VARCHAR(10)  NOT NULL
+);
+
+CREATE TABLE farm_fertilizer
+(
+    `farm_id`       VARCHAR(255) NOT NULL,
+    `fertilizer_id` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`farm_id`) REFERENCES farm (`id`),
+    FOREIGN KEY (`fertilizer_id`) REFERENCES fertilizer (`id`)
+);
+
+CREATE TABLE pesticide
+(
+    `id`        VARCHAR(255) PRIMARY KEY,
+    `name`      VARCHAR(255) NOT NULL,
+    `unit_type` VARCHAR(10)  NOT NULL
+);
+
+CREATE TABLE farm_pesticide
+(
+    `farm_id`      VARCHAR(255) NOT NULL,
+    `pesticide_id` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`farm_id`) REFERENCES farm (`id`),
+    FOREIGN KEY (`pesticide_id`) REFERENCES pesticide (`id`)
+);
