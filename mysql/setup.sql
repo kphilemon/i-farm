a3f3b751b96b2c38afd928c4f974b1268bce60f6
rachelong <57581517+rachelong@users.noreply.github.com>
2022-05-29 23:27:13 +0800

    Add create table statements for user, user_farm, activity

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -54,3 +54,36 @@ CREATE TABLE farm_pesticide
     FOREIGN KEY (`farm_id`) REFERENCES farm (`id`),
     FOREIGN KEY (`pesticide_id`) REFERENCES pesticide (`id`)
 );
+
+CREATE TABLE user
+(
+    `id`           VARCHAR(255) PRIMARY KEY,
+    `name`         VARCHAR(255) NOT NULL,
+    `email`        VARCHAR(255) NOT NULL,
+    `password`     VARCHAR(255) NOT NULL,
+    `phone_number` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_farm
+(
+    `user_id` VARCHAR(255) NOT NULL,
+    `farm_id` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user (`id`),
+    FOREIGN KEY (`farm_id`) REFERENCES farm (`id`)
+);
+
+CREATE TABLE activity
+(
+    `id`       VARCHAR(255) PRIMARY KEY,
+    `farm_id`  VARCHAR(255)     NOT NULL,
+    `user_id`  VARCHAR(255)     NOT NULL,
+    `date`     VARCHAR(255)     NOT NULL,
+    `action`   VARCHAR(50)      NOT NULL,
+    `type`     VARCHAR(255)     NOT NULL,
+    `unit`     VARCHAR(255)     NOT NULL,
+    `quantity` DOUBLE PRECISION NOT NULL,
+    `field`    INTEGER UNSIGNED NOT NULL,
+    `row`      INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (`farm_id`) REFERENCES farm (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES user (`id`)
+);
