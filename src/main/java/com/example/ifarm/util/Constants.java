81d640c4b9a8b1a2cc5c6b51b75e349aa896845d
nic-00 <55591460+nic-00@users.noreply.github.com>
2022-06-17 09:34:40 +0800

    Declare constants

---
+++ src/main/java/com/example/ifarm/util/Constants.java
@@ -0,0 +1,21 @@
+package com.example.ifarm.util;
+
+public class Constants {
+    public static final String ACTION_SOWING = "sowing";
+    public static final String ACTION_FERTILIZERS = "fertilizers";
+    public static final String ACTION_PESTICIDES = "pesticides";
+    public static final String ACTION_HARVEST = "harvest";
+    public static final String ACTION_SALES = "sales";
+    public static final String[] ACTION_LIST = new String[]{ACTION_SOWING, ACTION_FERTILIZERS, ACTION_PESTICIDES, ACTION_HARVEST, ACTION_SALES};
+
+    public static final String UNIT_KG = "kg";
+    public static final String UNIT_G = "g";
+    public static final String UNIT_PACK_500 = "pack (500g)";
+    public static final String UNIT_PACK_1000 = "pack (1000g)";
+    public static final String UNIT_L = "l";
+    public static final String UNIT_ML = "ml";
+
+    public static final String UNIT_TYPE_MASS = "mass";
+    public static final String UNIT_TYPE_PACK = "pack";
+    public static final String UNIT_TYPE_VOLUME = "volume";
+}
